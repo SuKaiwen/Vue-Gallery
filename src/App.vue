28733cadc3f3b9c35a3b7896d326bcd660be0690
Kevin Su <kevinsu@outlook.co.nz>
2021-09-26 17:03:14 +1300

    search working

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  
   <Body
     :photoArray = "photos.photoArray"
+    :message = "message"
   />
 </template>
 
@@ -20,19 +22,22 @@ export default {
         photoArray: [],
       },
 
-      user:{
-        userArray: [],
-      }
+      message: "Auckland",
     }
   },
 
   methods:{
-    async loadImg(){
-      const res = await fetch(`https://api.unsplash.com/search/photos/?query='auckland'&per_page=9&client_id=${process.env.VUE_APP_API_KEY}`);
+    async loadImg(term){
+      const res = await fetch(`https://api.unsplash.com/search/photos/?query='${term}'&per_page=9&client_id=${process.env.VUE_APP_API_KEY}`);
       const results = await res.json();
       const sorted = results.results.sort((a, b) => (a.height/a.width) > (b.height/b.width) ? -1: 1);
       this.photos.photoArray = sorted;
       console.log(this.photos.photoArray);
+    },
+
+    changeTerm(term){
+      this.message = term;
+      this.loadImg(term);
     }
   }
 }
@@ -46,5 +51,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-family:Source Sans Pro;
 }
 </style>
--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,33 +1,48 @@
 <template>
   <div>
-    <h1>Vuegram</h1>
-    <button v-on:click="loadImg">Tests results</button>
+    <h1 class = "display-1">VUEGRAM</h1>
+    <div class="search">
+    <form>
+        <input type="term" required v-model="term">
+    </form>
+    
+    <button v-on:click="changeTerm(term)">Search</button>
+    </div>
+    <div class="top">
+        <h1>Showing results for "{{message}}"... </h1>
+    </div>
     <div class="container">
         <div class="grid" >
             <div class="card-alt" v-for="photo in photoArray" :key="photo.id">
                 <img class="photo" :src="photo.urls.small" />
                 <div class="card-text">
-                    <h3 v-if="photo.alt_description !== null">{{photo.alt_description}}</h3>
-                    <h3 v-else>Unnamed</h3>
-                    <br />
-                    <p v-if="photo.description !== null">Description: {{photo.description}}</p>
-                    <p v-else>No Description</p>
-                    <br />
-                    <p>Created: {{photo.created_at}}</p>
-                    <p>Likes: {{photo.likes}}</p>
-                    <br />
-                    <p>Tags: </p>
-                    <hr />
-                    <div class="container">
-                        <div v-for="tags in photo.tags" :key="tags.title">
-                            <va-button size="small" color="info">{{tags.title}}</va-button>
-                        </div>
+                    <div class="top-part">
+                        <a :href="photo.links.html"><i class="fas fa-camera fa-2x"></i></a>
+                        <a :href="photo.links.download"><i class="fas fa-download fa-2x"></i></a>
                     </div>
-                    <hr />
-                    <p>Size: {{photo.width}}px x {{photo.height}}px</p>
-                    <br />
-                    <p>By {{photo.user.first_name}} {{photo.user.last_name}} | @{{photo.user.username}}</p>
-                    <br />
+                    <div>
+                        <h3 v-if="photo.alt_description !== null">{{photo.alt_description}}</h3>
+                        <h3 v-else>Unnamed</h3>
+                        <br />
+                        <p v-if="photo.description !== null">Description: {{photo.description}}</p>
+                        <p v-else>No Description</p>
+                        <br />
+                        <p>Created: {{photo.created_at}}</p>
+                        <p>Likes: {{photo.likes}}</p>
+                        <br />
+                        <p>Tags: </p>
+                        <hr />
+                        <div class="container">
+                            <div v-for="tags in photo.tags" :key="tags.title">
+                                <va-button size="small" color="info">{{tags.title}}</va-button>
+                            </div>
+                        </div>
+                        <hr />
+                        <p>Size: {{photo.width}}px x {{photo.height}}px</p>
+                        <br />
+                        <p>By {{photo.user.first_name}} {{photo.user.last_name}} | @{{photo.user.username}}</p>
+                        <br />
+                    </div>
                 </div>
             </div>
         </div>
@@ -40,11 +55,18 @@ export default {
   name: 'HelloWorld',
   props: {
     photoArray: Array,
+    message: String,
+  },
+
+  data(){
+      return{
+          term:""
+      }
   },
 
   methods:{
-    loadImg(){
-      this.$parent.loadImg();
+    changeTerm(term){
+      this.$parent.changeTerm(term); 
     },
   }
 }
@@ -63,7 +85,6 @@ export default {
         flex-direction: row;
         flex:3;
         flex-wrap: wrap;
-        padding:10px;
         max-width:80%;
         justify-content: space-evenly;
     }
@@ -84,20 +105,19 @@ export default {
 
     .card-alt:hover .card-text{
         opacity: 1;
-        display:block;
+        display:flex;
+        flex-direction: column;
         overflow: scroll;
         -ms-overflow-style: none;  /* Internet Explorer 10+ */
         scrollbar-width: none;  /* Firefox */
         height:100%;
+        background-color: rgba(0,0,0, 0.7);
+        color:white;
     }
 
     .card-text::-webkit-scrollbar { /* WebKit */
         width: 0;
         height: 0;
-    }
-
-    .card-alt:hover .photo{
-        opacity: 0.3;
     }
 
     .card-text{
@@ -112,11 +132,44 @@ export default {
         position:absolute;
         overflow: hidden;
         background-attachment: fixed;
+        justify-content: space-between;
+        
         display:none;
+    }
+
+    i{
+        padding:5px;
     }
 
     p{
         font-size: 14px;
+    }
+
+    a{
+        color:white;
+    }
+
+    a:hover{
+        color:white;
+    }
+
+    .top{
+        text-align: left;
+        max-width:80%;
+        margin:auto;
+        padding:2%;
+    }
+
+    .top-part{
+        text-align: right;
+        justify-content: space-between;
+    }
+
+    .search{
+        justify-content: center;
+        display:flex;
+        flex-direction: row;
+        margin-top:50px;
     }
 
     @media (min-width:0px) and (max-width:600px){
