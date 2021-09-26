cad4d95cc19cf8286477902149eea5f1cda1eb7f
Kevin Su <kevinsu@outlook.co.nz>
2021-09-26 14:18:46 +1300

    image and image details

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -2,18 +2,31 @@
   <div>
     <h1>Vuegram</h1>
     <button v-on:click="loadImg">Tests results</button>
-    <div v-for="photo in photoArray" :key="photo.id">
-        <img :src="photo.urls.small" />
-        <p>Description: {{photo.description}}</p>
-        <p>Alt: {{photo.alt_description}}</p>
-        <p>Created: {{photo.created_at}}</p>
-        <p>Likes: {{photo.likes}}</p>
-        <div v-for="tags in photo.tags" :key="tags.title">
-            <p>{{tags.title}}</p>
+    <div class="container">
+        <div class="grid">
+            <div class="card-alt" v-for="photo in photoArray" :key="photo.id">
+                <img class="photo" :src="photo.urls.small" />
+                <h3 v-if="photo.alt_description !== null">{{photo.alt_description}}</h3>
+                <h3 v-else>Unnamed</h3>
+                <br />
+                <p>Description: {{photo.description}}</p>
+                <p>Created: {{photo.created_at}}</p>
+                <p>Likes: {{photo.likes}}</p>
+                <br />
+                <p>Tags: </p>
+                <hr />
+                <div class="container">
+                    <div v-for="tags in photo.tags" :key="tags.title">
+                        <va-button size="small" color="info">{{tags.title}}</va-button>
+                    </div>
+                </div>
+                <hr />
+                <p>Size: {{photo.width}}px x {{photo.height}}px</p>
+                <br />
+                <p>By {{photo.user.first_name}} {{photo.user.last_name}} | @{{photo.user.username}}</p>
+                <br />
+            </div>
         </div>
-        <p>Size: {{photo.width}}px x {{photo.height}}px</p>
-        <p>By {{photo.user.first_name}} {{photo.user.last_name}} | @{{photo.user.username}}</p>
-        <br />
     </div>
   </div>
 </template>
@@ -34,5 +47,34 @@ export default {
 </script>
 
 <style scoped>
+    .container{
+        margin:auto;
+        width:100%;
+        display: flex;
+        justify-content: center;
+    }
+    
+    .grid{
+        display:flex;
+        flex-direction: row;
+        flex:3;
+        flex-wrap: wrap;
+        padding:10px;
+        max-width:70%;
+        justify-content: space-evenly;
+    }
 
+    .card-alt{
+        text-align: left;
+        align-items: center;
+        width:30%;
+    }
+
+    .photo{
+        width:100%;
+    }
+
+    p{
+        font-size: 14px;
+    }
 </style>
